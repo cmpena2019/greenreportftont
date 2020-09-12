d8ccdb23cb5e89ea87c849e93cb692a44f6c4edc
Cristian Bolivar <cambol1231@gmail.com>
2020-09-12 11:14:29 -0500

    actulizacion

--- src/app/registro/registro.component.ts
+++ src/app/registro/registro.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { PersonalService } from '../Servicios/personal.service';
+import { Personal } from '../models/Personal';
+import {StorageService} from '../Servicios/storage.service'
 
 @Component({
   selector: 'app-registro',
@@ -7,9 +12,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistroComponent implements OnInit {
 
-  constructor() { }
+  public mostrarNuevo: boolean;
+  public mostrarLista: boolean;
+  RegistroForm: FormGroup;
+  submitted = false;
+  returnUrl: string;
+  hasError: boolean;
+  errorMessage: string;
+  user: any = undefined;
+  personas: any = undefined;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private route: ActivatedRoute,
+    private router: Router,
+    //private api:PersonalService,
+    private storage: StorageService
+  ) {
+    this.mostrarNuevo = false;
+    this.mostrarLista = true;   
+  }
 
   ngOnInit() {
+    this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/home';
+
+    this.user = JSON.parse(localStorage.getItem('_user'));
+
+  }
+
+  mostrar(seleccionado: any) {
+    if (seleccionado === 'N') {
+      this.mostrarNuevo = true;
+      this.mostrarLista = false;
+    } else {
+      if (seleccionado === 'L') {
+        this.mostrarNuevo = false;
+        this.mostrarLista = true;
+      }
+    }
   }
 
 }
