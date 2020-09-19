592d82bc0e9bb1e22ed2869dea4f15c84da0523e
Cristian Bolivar <cambol1231@gmail.com>
2020-09-19 11:23:36 -0500

    Actulizacion Usuario

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,16 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
+import { Router, ActivatedRoute } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {StorageService} from '../Servicios/storage.service'
 
 @Component({ templateUrl: 'home.component.html' })
 export class HomeComponent implements OnInit {
     currentUserSubscription: Subscription;
 
+    user: any = undefined;
+
     constructor(
+        private storage: StorageService
     ) {
 
     }
 
     ngOnInit() {
+
+        this.user = JSON.parse(localStorage.getItem('_user'));
     }
 
     private loadAllUsers() {
